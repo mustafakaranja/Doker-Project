49a3e92601e1ea2211ec7fd22a23861600feeaf0
mustafakaranja <mustufakaranjawala72@gmail.com>
2021-03-09 18:12:49 +0530

    Docker project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./Assest/PhotoGlogo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> Hey! this is a project, to deploy using the docker image.</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
