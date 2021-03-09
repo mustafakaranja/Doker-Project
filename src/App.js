90630a3591e2c357a019c9c379e51af67b4cf940
mustafakaranja <mustufakaranjawala72@gmail.com>
2021-03-09 19:48:12 +0530

    add Link

--- src/App.js
+++ src/App.js
@@ -10,13 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="https://photogtech.in" target="_blank">
+          Photo-G Technologies
         </a>
       </header>
     </div>
